92d00f4f6a7032d7047ef4fdc72e50c50b5cc18c
magdalena krasicka <krasickamagdalena@gmail.com>
2023-08-08 16:57:01 +0100

    added button which clean ul

--- main.js
+++ main.js
@@ -3,19 +3,25 @@ let orderElement = 1;
 
 const init = () => {
   const btn = document.createElement('button');
+  const btnReset = document.createElement('button');
   document.body.appendChild(btn);
+  document.body.appendChild(btnReset);
   btn.style.width = 100 + 'px';
   btn.style.height = 50 + 'px';
+  btnReset.style.width = 100 + 'px';
+  btnReset.style.height = 50 + 'px';
   btn.textContent = "Create list"
+  btnReset.textContent = "Delete list"
   const ul = document.createElement('ul');
   document.body.appendChild(ul);
   btn.addEventListener("click", createLiElements);
+  btnReset.addEventListener("click", deleteLiElements);
 
  // tutaj kod
 }
 
 const createLiElements = () => {
-
+  for (i = 0; i < 10; i++) {
     const ul = document.querySelector('ul')
     const li = document.createElement('li');
     ul.appendChild(li);
@@ -24,8 +30,13 @@ const createLiElements = () => {
     size++
     li.textContent = `element${orderElement}`
     orderElement++
+  }
+}
 
- // tutaj kod
+const deleteLiElements = () => {
+  document.querySelector('ul').textContent = "";
+  size = 10;
+  orderElement = 1;
 }
 
 init()
